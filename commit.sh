e139cabe19babef223a7da62bad220a6c7537eb2
root <root@silence.me>
2018-01-11 09:59:37 +0800

    update: 2018-01-11 09:59:36

--- commit.sh
+++ commit.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
+# quick commit
+
+INFO=$1
+
+if [ ! -n "$INFO" ]; then
+	echo "you can commit with your update info, e.g.: ./commit.sh 'UPDATE INFO'"
+	INFO="update: `date '+%Y-%m-%d %H:%M:%S'`"
+fi
+
 git pull
 git add -A
-git commit -m "update: $1"
+git commit -m "$INFO"
 git push
